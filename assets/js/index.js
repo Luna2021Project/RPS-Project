90b2b6557f2791f63a6a7b1f7684083839271b21
Luna2021Project <lucianasnaves@yahoo.com.br>
2022-03-23 12:01:51 +0000

    teste

--- assets/js/index.js
+++ assets/js/index.js
@@ -59,3 +59,17 @@ const getUserChoice = function userInput() {
   }
   
   playGame();
+
+
+  function buttonClicked(event) {
+    if (event.target.aria-label === "Rock") {
+        console.log("Deu certo");
+    }
+      /*  event.target.style.backgroundColor = "orange";
+    } else {
+        event.target.style.backgroundColor = "green";
+    } */
+ }
+    let button = document.getElementsByClassName("button");
+      for (let i = 0; i < button.length; i++)
+      button[i].addEventListener('click', buttonClicked);
